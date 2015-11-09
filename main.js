928372d0fa32dacdf51a2ffa4e065d6a8de904a4
Kate Wilson <KateWilson@Kates-MacBook-Pro.local>
2015-11-09 13:16:47 -0500

    working now

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var data;
 $(document).ready(function () {
   page.init();
 });
@@ -5,50 +6,68 @@ $(document).ready(function () {
 var page = {
   url: 'http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=e70dcfdcf18b2523bd529274a81155ae%3A8%3A73361668',
   init: function () {
-  page.getMostPop;
-  page.myEach;
+    page.getMostPop();
+    page.myEach();
   },
 
-getMostPop: function () {
+  getMostPop: function () {
       $.ajax({
         dataType: "json",
         url: 'http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=e70dcfdcf18b2523bd529274a81155ae%3A8%3A73361668',
         method: "GET",
-        data: page,
+        data: 'JSONP',
         success: function(myData){
-          console.log(myData.url);
+          data = myData.results;
+          console.log("SUCCESS: ", myData);
+          myData.results.forEach(function(el,idx,arr) {
+            var uRL = el.url;
+            var theTitle = el.title;
+            var byline = el.byline;
+            $('.mostPop').append("<h2><a href='" + uRL+ "'>" + theTitle + "</a></h2>" + "<h4>"+ byline + "</h4>");
+
+
+
+          })
+
+
+
+        },
+        error: function(myData) {
+          console.log("FAILURE: ", myData);
+          data = myData;
         }
       });
-    }
-  };
-myEach: function(){
-var dataRes = data.results
+    },
+    myEach: function() {
+      // var dataRes = data.results
+      //
+      // var uRL = $.each( dataRes, function(){
+      //   for (var i=0; i< dataRes.length; i++)
+      //       for (var name in dataRes[i]) {
+      //           $(".link").append(dataRes[i].url);
+      //       }
+      //     });
+      //   var theTitle = $.each( dataRes, function(){
+      //     for (var i=0; i< dataRes.length; i++)
+      //         for (var name in dataRes[i]) {
+      //             $(".title").append(dataRes[i].title);
+      //         }
+      //       });
+      //   var byLine = $.each( dataRes, function(){
+      //     for (var i=0; i< dataRes.length; i++)
+      //         for (var name in dataRes[i]) {
+      //             $(".byline").append(dataRes[i].byline);
+      //       }
+      //   });
+      //
+      // $('.mostPop').append(uRL + theTitle + byline);
+    },
 
-var uRL = $.each( dataRes, function(){
-  for (var i=0; i< dataRes.length; i++)
-      for (var name in dataRes[idx]) {
-          $(".link").append(dataRes[idx].url);
-      }
-    });
-  var theTitle = $.each( dataRes, function(){
-      for (var i=0; i< dataRes.length; i++)
-          for (var name in dataRes[idx]) {
-              $(".title").append(dataRes[idx].title);
-          }
-        });
-        var byLine = $.each( dataRes, function(){
-          for (var i=0; i< dataRes.length; i++)
-              for (var name in dataRes[idx]) {
-                  $(".byline").append(dataRes[idx].byline);
-              }
-            });
 
-        $('.mostPop').append(url + theTitle + byline);
-});
+  }
     // data.results[0].url
     // data.results[0].title
     // data.results[0].byline
-
 
 
 // console.log(data.id);
